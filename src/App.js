b4ee90b253f841ea103deca916949198c2102a04
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-10-23 22:26:33 -0400

    addNavBar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Resume from './pages/Resume'
 import Projects from './pages/Projects'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
 
