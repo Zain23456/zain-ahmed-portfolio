093b2dc2be050ef02718e4876364582aac3f0166
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-07 09:50:01 -0500

    stub up projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+const projects = [
+  {
+    title: 'Avi',
+    description: 'A birding community oriented application that allows birds to share and connect. In this group project I was the git manger',
+    image:'',
+    repositoryLink: '',
+    deploymentLink: '',
+  },
+  {
+    title: 'Trip',
+    description: 'A travel communit oriented application that allows user to share their experiences and recommend locations and activities to visit. This is a MEN stack application.',
+    image:'',
+    repositoryLink: '',
+    deploymentLink: '',
+  },
+  {
+    title: 'Connect Four',
+    description: 'A vanilla Javascript game to enjoy with family and friends when you feel to lazy to move off the couch.',
+    image:'',
+    repositoryLink: '',
+    deploymentLink: '',
+  },
+  {
+    title: 'Tic-tac Toe',
+    description: '',
+    image:'',
+    repositoryLink: '',
+    deploymentLink: '',
+  }
+]
