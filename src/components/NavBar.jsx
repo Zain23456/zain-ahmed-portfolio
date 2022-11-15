fd0f5530c9efc1e345930280a4aa496658e2cf33
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-14 21:24:16 -0500

    update style for nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,19 +4,21 @@ import styles from './NavBar.module.css'
 function NavBar() {
   return (
     <nav className={styles.navbar}>
-      <Link to='/'>Home</Link>
       <ul>
         <li>
-          <Link to='/about'>About</Link>
+          <Link to='/' className={styles.link}>Home</Link>
         </li>
         <li>
-          <Link to='/resume'>Resume</Link>
+          <Link to='/about' className={styles.link}>About</Link>
         </li>
         <li>
-          <Link to='/projects'>Projects</Link>
+          <Link to='/projects' className={styles.link}>Projects</Link>
         </li>
         <li>
-          <Link to='/contact'>Contact</Link>
+          <Link to='/resume' className={styles.link}>Resume</Link>
+        </li>
+        <li>
+          <Link to='/contact' className={styles.link}>Contact</Link>
         </li>
       </ul>
     </nav>
