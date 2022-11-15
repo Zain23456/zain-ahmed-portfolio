aa78302205cb83e19d02f1e1e5d50dadeb4a4b24
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-14 21:37:07 -0500

    add styles for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import Image from '../assets/myPhoto.jpg'
+import styles from './Home.module.css'
 
 function Home() {
 
   return (
-    <div>
+    <div className={styles.body}>
       <h1>Zain Ahmed</h1>
       <img src={Image} alt='Zain Ahmed'/>
       <p>Software Engineer</p>
